d0e12a5210a7e9728f4b2cc2dbcf67d0b1962684
Yujaebin <dbwpqls9998@naver.com>
2023-01-29 23:14:08 +0900

    Chore: 초기세팅

---
+++ front/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div>hellow world</div>
+      <div
+        style={{
+          width: "5px",
+          height: "50px",
+          backgroundColor: "peru",
+          transform: "rotate(-45deg)",
+        }}
+      ></div>
+    </>
+  );
+};
+
+export default Login;
