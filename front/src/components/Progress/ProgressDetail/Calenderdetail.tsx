9a6c2eeb84ec2d761a52c78c109be1151f691314
seoyoungde <76784522+seoyoungde@users.noreply.github.com>
2023-04-06 06:24:23 +0900

    react calendar

--- front/src/components/Progress/ProgressDetail/Calenderdetail.tsx
+++ front/src/components/Progress/ProgressDetail/Calenderdetail.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Cal from "../ProgressDetail/cal";
 
 const Calenderdetail = () => {
   return (
@@ -11,18 +12,15 @@ const Calenderdetail = () => {
             fontSize: "1.2rem",
             fontWeight: "bold",
             marginLeft: "1rem",
+            marginBottom: "2rem",
           }}
         >
           일정
         </p>
-        <Tabbox>11</Tabbox>
-        <Tablebox>22</Tablebox>
+        <Tablebox>
+          <Cal></Cal>
+        </Tablebox>
       </Calenderbox>
-      <Calendershpae>
-        <p>캘린더</p>
-        <Calenderday>111</Calenderday>
-        <Calendertodo>22</Calendertodo>
-      </Calendershpae>
     </CalenderForm>
   );
 };
@@ -31,33 +29,13 @@ const CalenderForm = styled.div`
   flex-direction: row;
 `;
 const Calenderbox = styled.div`
-  width: 70%;
-  height: 82vh;
-  border: 3px solid red;
-`;
-const Calendershpae = styled.div`
-  width: 30%;
-  border: 3px solid blue;
-`;
-const Tabbox = styled.div`
   width: 100%;
-  height: 5vh;
-  margin-top: 1.7rem;
-  background-color: #f5f5f5;
+  height: 82vh;
 `;
+
 const Tablebox = styled.div`
   width: 100%;
-
   background-color: yellow;
 `;
-const Calenderday = styled.div`
-  width: 100%;
-  height: 30vh;
-  background-color: blue;
-`;
-const Calendertodo = styled.div`
-  width: 100%;
-  height: 20vh;
-  background-color: purple;
-`;
+
 export default Calenderdetail;
