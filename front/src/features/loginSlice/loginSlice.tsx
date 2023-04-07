acb3ded28f94f37c1e14627959a2d8b6dba4a3ae
Yujaebin <dbwpqls9998@naver.com>
2023-04-07 15:24:28 +0900

    fix2

---
+++ front/src/features/loginSlice/loginSlice.tsx
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+interface loginSuccess {
+  message: string;
+  object: number | null;
+  status: string;
+}
+interface loginInfo {
+  email: string;
+  password: string;
+  user_gb: string;
+}
+const initialState: loginSuccess = {
+  message: "",
+  object: null,
+  status: "",
+};
+export const loginAsync = createAsyncThunk<loginSuccess, loginInfo>(
+  "login",
+  async (loginData) => {
+    try {
+      const { data } = await axios({
+        url: "/api/v1/login",
+        method: "post",
+        data: {
+          email: `${loginData.email}`,
+          password: `${loginData.password}`,
+          user_gb: `${loginData.user_gb}`,
+        },
+      });
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+export const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loginAsync.fulfilled, (state, { payload }) => {
+      state.message = payload.message;
+      state.object = payload.object;
+      state.status = payload.status;
+      console.log(state.message);
+      console.log(state.object);
+      console.log(state.status);
+    });
+  },
+});
+export default loginSlice.reducer;
